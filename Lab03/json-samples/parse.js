dcbd1d4fba9961358481aa9a593a3bfc8395c216
Crazy Cat <101245572+mazecate@users.noreply.github.com>
2023-06-14 01:27:41 +0800

    Delete Lab03 directory

--- Lab03/json-samples/parse.js
+++
@@ -1,7 +0,0 @@
-const x = '{"id": "AB0000000", "name": "John", "age": 21}';  // x is string
-var person = JSON.parse(x);  // person is a JSON object
-
-console.log(`${person.name} is aged ${person.age}`);  // print person as JSON
-
-var y = JSON.stringify(person);  // y is string
-console.log(y);
