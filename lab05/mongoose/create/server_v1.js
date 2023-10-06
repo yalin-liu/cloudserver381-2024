f56274e15325216cd8e8e6a347436cc0d076434b
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2023-10-06 09:22:44 +0800

    Update server_v1.js

--- lab05/mongoose/create/server_v1.js
+++ lab05/mongoose/create/server_v1.js
@@ -5,7 +5,7 @@ const kittySchema = require('./models/kitty');
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', async (callback) => { // javascript - Mongoose stopped accepting callbacks for some of its functions - Stack Overflow
+db.once('open', async (callback) => { 
 	let Kitten = mongoose.model('Kitten', kittySchema);
 	try {
 		let fluffy = new Kitten({name: 'fluffy', age: 5});
@@ -22,4 +22,6 @@ db.once('open', async (callback) => { // javascript - Mongoose stopped accepting
 	// 	console.log('Kitten created!')
 	// 	db.close();
 	// });
+	// javascript - Mongoose stopped accepting callbacks for some of its functions - Stack Overflow
+	// https://stackoverflow.com/questions/75586474/mongoose-stopped-accepting-callbacks-for-some-of-its-functions
 });
