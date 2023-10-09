3770fc48435b4bc1c0d8ac2d35197b8f67385c19
mazeteam2013 <mazeteam2013@gmail.com>
2023-10-10 00:22:36 +0800

    update lab05 example2

--- lab05/base64/example2/server.js
+++ lab05/base64/example2/server.js
@@ -3,10 +3,10 @@ const url = require('url');
 const fs = require('fs');
 const formidable = require('formidable');
 const MongoClient = require('mongodb').MongoClient;
-// Alin: The old mongodb module version 3.6.5, the latest mongodb module version 6.1.0, see more details in https://www.npmjs.com/package/mongodb
 const assert = require('assert');
 const mongourl = "";
 const dbName = "";
+const collectionName = "photo";
 
 const server = http.createServer((req, res) => {
   let timestamp = new Date().toISOString();
@@ -14,8 +14,7 @@ const server = http.createServer((req, res) => {
 
   let parsedURL = url.parse(req.url,true); // true to get query as object
 
-  if (parsedURL.pathname == '/fileupload' && 
-      req.method.toLowerCase() == "post") {
+  if (parsedURL.pathname == '/fileupload' && req.method.toLowerCase() == "post") {
     // parse a file upload
     const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
@@ -23,31 +22,34 @@ const server = http.createServer((req, res) => {
         res.writeHead(500,{"Content-Type":"text/plain"});
         res.end("No file uploaded!");  
       }
+
       const filename = files.filetoupload.path;
       const title = (fields.title.length > 0) ? fields.title : "untitled";
       const mimetype = files.filetoupload.type;
-      fs.readFile(filename, (err,data) => {
+
+      fs.readFile(filename, async (err,data) => {
         const client = new MongoClient(mongourl);
-        client.connect((err) => {
-          try {
-            assert.equal(err,null);
-          } catch (err) {
-            res.writeHead(500,{"Content-Type":"text/plain"});
-            res.end("MongoClient connect() failed!");
-            return(-1);
-          }
+
+        try {
+          await client.connect();
           console.log('Connected to MongoDB server.')
+          
           const db = client.db(dbName);
           const new_r = {};  // new document to be inserted
           new_r['title'] = title;
           new_r['mimetype'] = mimetype;
-          new_r['image'] = new Buffer(data).toString('base64');
-          insertPhoto(db,new_r,(result) => {
-            client.close();
+          new_r['image'] = Buffer.from(data, 'base64');
+
+          var insertResult = await insertPhoto(db, new_r);
+          if (!insertResult) {
             res.writeHead(200, {"Content-Type": "text/plain"});
             res.end('Photo was inserted into MongoDB!');
-          })
-        });
+          }
+        } catch (err) {
+          console.error(err);
+        } finally {
+          client.close();
+        }
       })
     });
   } else {
@@ -61,13 +63,17 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const insertPhoto = (db,r,callback) => {
-  db.collection('photo').insertOne(r,function(err,result) {
-    assert.equal(err,null);
+const insertPhoto = async (db,r) => {
+  try {
+    var insertPhotoResult = await db.collection(collectionName).insertOne(r);
+    console.log(insertPhotoResult);
     console.log("insert was successful!");
-    console.log(JSON.stringify(result));
-    callback(result);
-  });
+    console.log(JSON.stringify(insertPhotoResult));
+    return false;
+  } catch (err) {
+    console.error(err);
+    return true;
+  }
 }
 
 server.listen(process.env.PORT || 8099);
