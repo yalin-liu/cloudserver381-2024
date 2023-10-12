141faca3f0a49e4801ec7148197aa4a302a0b658
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2023-10-12 18:07:40 +0800

    Update Practical3.js

--- lab05/4b/Practical3.js
+++ lab05/4b/Practical3.js
@@ -1,4 +1,4 @@
-const ObjectID = require('mongodb').ObjectID;
+const { ObjectId } = require('mongodb');
 const assert = require('assert');
 const http = require('http');
 const url = require('url');
@@ -11,103 +11,177 @@ const bookingSchema = mongoose.Schema({
 });
 
 const handle_Find = (res, criteria) => {
-    mongoose.connect(mongourl, {useMongoClient: true});
+    mongoose.connect(mongourl);// remove , {useMongoClient: true}
+
     let db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
+    db.once('open', async () => { //async
+		
         const Booking = mongoose.model('booking',bookingSchema);
-        Booking.find(criteria, (err,results) => {
-            if (err) return console.error(err);
-            res.writeHead(200, {"content-type":"text/html"});
-            res.write(`<html><body><H2>Bookings (${results.length})</H2><ul>`);
-            for (var doc of results) {
+        let findResult = [];
+
+		try {
+			findResult = await Booking.find(criteria).exec();
+		} catch (err) {
+			console.error(err);
+		} finally {
+			res.writeHead(200, {"content-type":"text/html"});
+            res.write(`<html><body><H2>Bookings (${findResult.length})</H2><ul>`);
+            for (var doc of findResult) {
                 res.write(`<li>Booking ID: <a href="/details?_id=${doc._id}">${doc.bookingid}</a></li>`);
             }
             res.end('</ul></body></html>');
             console.log("Closed DB connection");
             db.close();
-        })
+		}
+		        
+		//Booking.find(criteria, (err,results) => {
+        //    if (err) return console.error(err);
+        //    res.writeHead(200, {"content-type":"text/html"});
+        //    res.write(`<html><body><H2>Bookings (${results.length})</H2><ul>`);
+        //    for (var doc of results) {
+        //        res.write(`<li>Booking ID: <a href="/details?_id=${doc._id}">${doc.bookingid}</a></li>`);
+        //    }
+        //    res.end('</ul></body></html>');
+        //    console.log("Closed DB connection");
+        //    db.close();
+        //})
     })
 }
 
 const handle_Details = (res, criteria) => {
-    mongoose.connect(mongourl, {useMongoClient: true});
+    mongoose.connect(mongourl);// remove , {useMongoClient: true}
 
     let db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
+    db.once('open', async () => { //async
         /* use Document ID for query */
         let DOCID = {};
-        DOCID['_id'] = ObjectID(criteria._id)
+        DOCID['_id'] = new ObjectId(criteria._id)
         const Booking = mongoose.model('booking',bookingSchema);
-        Booking.findOne(criteria, (err,results) => {
-            if (err) return console.error(err);
-            res.writeHead(200, {"content-type":"text/html"});
-            res.write('<html><body><ul>');
-            //console.log(docs);
-            res.write(`<H2>Booking Details</H2><hr>`)
-            res.write(`<p>Booking ID: <b>${results.bookingid}</b></p>`);
-            res.write(`<p>Mobile: <b>${results.mobile}</b></p>`)
+        let findResult = {};
+
+		try {
+			findResult = await Booking.findOne(criteria);//.exec();
+		} catch (err) {
+			console.error(err);
+		} finally {
+			res.writeHead(200, {"content-type":"text/html"});
+			res.write('<html><body><ul>');
+			res.write(`<H2>Booking Details</H2><hr>`);
+			res.write(`<p>Booking ID: <b>${findResult.bookingid}</b></p>`);
+			res.write(`<p>Mobile: <b>${findResult.mobile}</b></p>`);
+			res.write(`<a href="/edit?_id=${findResult._id}">edit</a><br><br>`)
+			res.write(`<a href="/find">back<a>`);
+			res.end('</body></html>');
+			db.close();
+		}
+		// Booking.findOne(criteria, (err,results) => {
+        //    if (err) return console.error(err);
+        //    res.writeHead(200, {"content-type":"text/html"});
+        //    res.write('<html><body><ul>');
+        //    //console.log(docs);
+        //    res.write(`<H2>Booking Details</H2><hr>`)
+        //    res.write(`<p>Booking ID: <b>${results.bookingid}</b></p>`);
+        //    res.write(`<p>Mobile: <b>${results.mobile}</b></p>`)
             // Q1
-            res.write(`<a href="/edit?_id=${results._id}">edit</a><br><br>`)
+        //    res.write(`<a href="/edit?_id=${results._id}">edit</a><br><br>`)
             //
-            res.write(`<a href="/find">back<a>`);
-            res.end('</body></html>');
-            db.close();
-        });
+        //    res.write(`<a href="/find">back<a>`);
+        //    res.end('</body></html>');
+        //    db.close();
+        // });
     });
 }
 
 const handle_Edit = (res, criteria) => {
-    mongoose.connect(mongourl, {useMongoClient: true});
+    mongoose.connect(mongourl);// remove , {useMongoClient: true}
     
     let db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
+    db.once('open', async () => { //async
         /* use Document ID for query */
         let DOCID = {};
-        DOCID['_id'] = ObjectID(criteria._id)
+        DOCID['_id'] = new ObjectId(criteria._id)
         const Booking = mongoose.model('booking',bookingSchema);
-        Booking.findOne(criteria, (err,results) => {
-            if (err) return console.error(err);
-            res.writeHead(200, {"content-type":"text/html"});
-            res.write('<html><body>');
-            res.write('<form action="/update" method=GET>');
-            res.write(`Booking ID: <input name="bookingid" value=${results.bookingid}><br>`);
-            res.write(`Mobile: <input name="mobile" value=${results.mobile} /><br>`);
-            res.write(`<input type="hidden" name="_id" value=${results._id}>`)
-            res.write(`<input type="submit" value="update">`);
-            res.end('</form></body></html>');
-            db.close();
-        });
+        let findResult = {}
+        try {
+			findResult = await Booking.findOne(criteria);//.exec();
+		} catch (err) {
+			consolde.error(err);
+		} finally {
+			res.writeHead(200, {"content-type":"text/html"});
+			res.write('<html><body>');
+			res.write('<form action="/update" method=GET>');
+			res.write(`Booking ID: <input name="bookingid" value=${findResult.bookingid}><br>`);
+			res.write(`Mobile: <input name="mobile" value=${findResult.mobile} /><br>`);
+			res.write(`<input type="hidden" name="_id" value=${findResult._id}>`)
+			res.write(`<input type="submit" value="update">`);
+			res.end('</form></body></html>');
+			db.close();
+		}
+		// Booking.findOne(criteria, (err,results) => {
+        //    if (err) return console.error(err);
+        //    res.writeHead(200, {"content-type":"text/html"});
+        //    res.write('<html><body>');
+        //    res.write('<form action="/update" method=GET>');
+        //    res.write(`Booking ID: <input name="bookingid" value=${results.bookingid}><br>`);
+        //    res.write(`Mobile: <input name="mobile" value=${results.mobile} /><br>`);
+        //    res.write(`<input type="hidden" name="_id" value=${results._id}>`)
+        //    res.write(`<input type="submit" value="update">`);
+        //    res.end('</form></body></html>');
+        //    db.close();
+        //});
     });
 }
 
 const handle_Update = (res, criteria) => {
-    mongoose.connect(mongourl, {useMongoClient: true});
+    mongoose.connect(mongourl);//remove , {useMongoClient: true}
     
-    let db = mongoose.connection;
+    const db = mongoose.connection;//let > const
 
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
+    db.once('open', async () => {//async
         /* use Document ID for query */
         let DOCID = {};
-        DOCID['_id'] = ObjectID(criteria._id)
+
+        // one way
+        // DOCID['_id'] = new mongoose.Types.ObjectId(criteria._id)
+
+        // second way
+        DOCID['_id'] = (criteria._id).toString();
+
         const Booking = mongoose.model('booking',bookingSchema);
-        Booking.findOne(DOCID, (err,results) => {
+        let findResult = {};
+		//update 
+		try {
+			findResult = await Booking.findOne(DOCID);//.exec();
+			findResult.bookingid = criteria.bookingid;
+			findResult.mobile = criteria.mobile;
+			await findResult.save();
+		} catch (err) {
+			console.error(err);		
+		} finally {
+			res.writeHead(200, {"content-type":"text/html"});
+			res.write(`<html><body><p>Updated document(s)<p><br>`);
+			res.end('<a href="/">back</a></body></html>');
+            db.close();		
+		}
+    
+		// Booking.findOne(DOCID, (err,results) => {
             //console.log(results);
-            results.bookingid = criteria.bookingid;
-            results.mobile = criteria.mobile;
-            results.save(err => {
-                res.writeHead(200, {"content-type":"text/html"});
-                res.write(`<html><body><p>Updated document(s)<p><br>`);
-                res.end('<a href="/">back</a></body></html>');
-                db.close();
-            })
-        })
+        //    results.bookingid = criteria.bookingid;
+        //    results.mobile = criteria.mobile;
+        //    results.save(err => {
+        //        res.writeHead(200, {"content-type":"text/html"});
+        //        res.write(`<html><body><p>Updated document(s)<p><br>`);
+        //        res.end('<a href="/">back</a></body></html>');
+        //        db.close();
+        //    })
+        // })
     })
 }
 
