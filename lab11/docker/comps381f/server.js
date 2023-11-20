de10519270206f3ce5364adb3552401ae902f318
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2023-11-20 10:00:04 +0800

    Delete lab11/docker directory

--- lab11/docker/comps381f/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req,res) => {
-  res.status(200).end(`Node version: ${process.version}\n`);
-});
-
-app.listen(process.env.PORT || 8099);
