dcdaa2d6d442d5a33a242cc1382425c3ba7a3464
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-09-23 02:26:04 +0800

    Create server.js

---
+++ lab04/mongodb-driver/test/server.js
@@ -0,0 +1,23 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = '';  
+const client = new MongoClient(url);
+const dbName = '';
+const collectionName = '';
+
+const countRestaurants = async (db) => {
+	var collection = db.collection(collectionName);
+	const count = await collection.countDocuments();
+	console.log(`There are ${count} documents in the ${collectionName} collection of the ${dbName} datababe.`);
+}
+
+async function main() {
+    await client.connect();	
+	console.log("You successfully connected to MongoDB!");
+    const db = client.db(dbName);
+	await countRestaurants(db);
+}
+
+main()
+  .then(console.log("Running the first MongoDB-driver test! ------Alin (This is an async running result.)"))
+  .catch(console.error)
+  .finally(() => client.close());
