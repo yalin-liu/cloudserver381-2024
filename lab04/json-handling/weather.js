6b121ccd07e3d4c134752f98fa81d951ad3f277b
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-09-23 01:19:59 +0800

    Create weather.js

---
+++ lab04/json-handling/weather.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const APIKEY='';//**your API key***
+const options = {
+    host: 'api.openweathermap.org',
+    port: 80,
+    path: '/data/2.5/weather?q=Tokyo,jp&units=metric&APPID=' + APIKEY,
+    method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+    res.on('data', (data) => {
+		console.log(data);
+    });
+});
+
+req.on('error', (error) => {
+    console.log(`Problem with request: ${error.message}`);
+});
+
+req.end();
