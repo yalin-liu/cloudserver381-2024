a19140b8edc1b84133bfa30e7a7a19191eceb147
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-09-23 01:18:16 +0800

    Delete lab04-2023 directory

--- lab04-2023/asyn-nodejs/async.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-fs.readFile('input.txt', (err, data) => {
-   if (err) return console.error(err);
-   console.log(data.toString());
-});
-
-console.log('Program Ended')
--- lab04-2023/asyn-nodejs/sync.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-// Sync version
-const data = fs.readFileSync('input.txt')
-
-console.log(data.toString())
-console.log('Program Ended')
-
