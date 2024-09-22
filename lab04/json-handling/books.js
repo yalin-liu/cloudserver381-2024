1fe5445189a988fc9e26f2d805c2f469c748e074
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-09-23 02:11:57 +0800

    Create books.js

---
+++ lab04/json-handling/books.js
@@ -0,0 +1,28 @@
+const books = [ 
+	{name: "Pascal Made Simple", 
+	 price: 700, 
+	 stock: 3,
+	 reviews: [
+	    {date:'2015-01-01T14:00:00Z',score:8},
+	    {date:'2015-02-01T08:00:00Z',score:7.5}
+	 ]ss},
+	{name: "Intro to the MEAN stack", 
+	 price: 300, 
+	 stock: 30,
+	 reviews: [
+	    {date:'2015-01-01T14:00:00Z',score:8}
+	 ]},
+	{name: "Node.js Programming", 
+	 price: 1000, 
+	 stock: 3,
+	 reviews: [
+	    {date:'2015-01-01T14:00:00Z',score:8},
+	    {date:'2015-02-01T08:00:00Z',score:7.5},
+	    {date:'2015-02-01T09:00:00Z',score:9.5}
+	 ]},
+	{name: "What is NOSQL?", 
+	 price: 900, 
+	 stock: 3000,
+	 reviews: [
+	 ]}
+];   
