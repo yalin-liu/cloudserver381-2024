4256c27362f62dfbd8101c84f1f541452d05a380
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-10-13 23:24:40 +0800

    Create template.js

---
+++ lab06/mongoose/template.js
@@ -0,0 +1,22 @@
+// Load "mongoose" modules
+const mongoose = require('mongoose');
+
+// Fill in uri connected to MongoDB, the same uri used in the mongodb driver
+const uri = '';
+
+// Data modelling 
+// Way 1: building new scheme in server.js
+// const kittySchema = new mongoose.Schema({name: String, age: Number});
+// Way 2: invoking your scheme defined in kitty.js 
+// const kittySchema = require('./models/kitty’);
+
+async function main() {
+  await mongoose.connect(uri);
+  console.log('Mongoose Connected!’)
+  // do some data operations
+}
+
+main()
+  .then(console.log('Kitten created'))
+  .catch(console.log(err))
+  .finally()
