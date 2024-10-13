d332b9d2c11174fe7a15e6eb6cc4b077144d7cc4
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-10-14 00:20:39 +0800

    Create server.js

---
+++ lab06/express/greetings/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const greetingMsg = (name = null, includeTime = false) => {
+  let today = new Date();
+  let msg = (name != null) ? 'Hello ' + name + '! ' : 'Hello there!';
+  if (includeTime) {
+    msg += `  It is now ${today.toTimeString()}`;
+    msg = `<html><head><title>sayHello</title></head><body><H1>${msg}</H1></body><html>`;
+  }
+  return(msg);
+}
+
+app.get('/greetings', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.status(200).end(greetingMsg(req.query.name, false));
+});
+
+app.get('/*', (req, res) => {  // default route for anything else
+  res.set('Content-Type', 'text/plain');
+  res.status(404).end("404 Not Found");
+})
+
+const server = app.listen(process.env.PORT || 8099, () => {
+  const port = server.address().port;
+  console.log(`Server listening at port ${port}`);
+});
