2efd386e7eea8ef5da4e4243afab32e97ce5503c
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-10-20 19:51:16 +0800

    Create server.js

---
+++ lab07/express-middleware/template/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use((req,res,next) => {
+  console.log(req.method + ' ' + req.url +
+              ' was requested at ' + Date(Date.now()).toString());
+  next();
+})
+
+app.get('/', (req, res, next) => {
+  res.send('Hello World!');
+});
+
+app.get('/help', (req, res, next) => {
+  res.send('Nope.. nothing to see here');
+});
+
+app.listen(process.env.PORT || 8099);
