830cb53ea814057d8f2d403665b2c08e254784f5
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2024-10-27 12:45:12 +0800

    Create server.js

---
+++ lab08/template/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+class SimpleInterest {
+	constructor(p,r,t) {
+		this.principal = p;
+		this.rate = r;
+		this.time = t;
+		this.interest = p * r * t;
+	}
+}
+
+// Web service testing: curl localhost:8099/simpleinterest/10000/0.01/10
+// Restful service testing: curl -H "accept: application/json" "localhost:8099/simpleinterest/10000/0.01/10"
+app.get('/simpleinterest/:principal/:rate/:time', function(req,res) {
+	let r = new SimpleInterest(Number(req.params.principal), Number(req.params.rate), Number(req.params.time));
+	if (req.headers['accept'] == 'application/json') {		
+		res.status(200).json(r);
+	} else {
+		res.status(200).render('result',{result:r});
+	}
+});
+
+app.listen(process.env.PORT || 8099);
